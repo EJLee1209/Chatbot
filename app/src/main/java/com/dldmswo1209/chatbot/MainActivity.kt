be554d38d23efc7c6d310a27c4e591b70b9f9583
EJLee1209 <dldmswo1209@gmail.com>
2022-08-02 15:26:02 +0900

    update-menu

--- app/src/main/java/com/dldmswo1209/chatbot/MainActivity.kt
+++ app/src/main/java/com/dldmswo1209/chatbot/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -14,24 +15,36 @@ import com.dldmswo1209.chatbot.todayTodo.TodoFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val homeFragment = HomeFragment()
+
         val todoFragment = TodoFragment()
+        val homeFragment = HomeFragment()
         val calendarFragment = CalendarFragment()
 
         replaceFragment(homeFragment)
-        binding.bottomNavigationView.selectedItemId = R.id.home
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home -> replaceFragment(homeFragment)
-                R.id.todayTodo -> replaceFragment(todoFragment)
-                R.id.emotionCalendar -> replaceFragment(calendarFragment)
-            }
-            true
+        binding.HomeButton.setOnClickListener {
+            replaceFragment(homeFragment)
+            binding.selectRectangleHome.isVisible = true
+            binding.selectRectangleTodo.isVisible = false
+            binding.selectRectangleCalendar.isVisible = false
         }
+        binding.todoButton.setOnClickListener {
+            replaceFragment(todoFragment)
+            binding.selectRectangleHome.isVisible = false
+            binding.selectRectangleTodo.isVisible = true
+            binding.selectRectangleCalendar.isVisible = false
+        }
+        binding.calendarButton.setOnClickListener {
+            replaceFragment(calendarFragment)
+            binding.selectRectangleHome.isVisible = false
+            binding.selectRectangleTodo.isVisible = false
+            binding.selectRectangleCalendar.isVisible = true
+        }
+
 
     }
     private fun replaceFragment(fragment: Fragment){
