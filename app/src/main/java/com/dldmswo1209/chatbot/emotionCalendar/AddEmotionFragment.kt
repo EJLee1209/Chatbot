d529cdec9534912a97cfddc999fc768b5a0f1726
LeeEunJae <101651909+EJLee1209@users.noreply.github.com>
2022-08-22 19:47:57 +0900

    오늘의 기분 추가하기 레이아웃 수정

--- app/src/main/java/com/dldmswo1209/chatbot/emotionCalendar/AddEmotionFragment.kt
+++ app/src/main/java/com/dldmswo1209/chatbot/emotionCalendar/AddEmotionFragment.kt
@@ -1,11 +1,15 @@
 package com.dldmswo1209.chatbot.emotionCalendar
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import com.dldmswo1209.chatbot.MainActivity
 import com.dldmswo1209.chatbot.R
 import com.dldmswo1209.chatbot.databinding.FragmentAddEmotionBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class AddEmotionFragment : Fragment(R.layout.fragment_add_emotion) {
     private lateinit var binding: FragmentAddEmotionBinding
@@ -15,11 +19,87 @@ class AddEmotionFragment : Fragment(R.layout.fragment_add_emotion) {
         binding = FragmentAddEmotionBinding.bind(view)
         calendarFragment = EmotionCalendar()
 
+        val current = LocalDate.now()
+        Log.d("testt", current.toString())
+        val formattedDate = current.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"))
+
+        binding.todayDate.text = formattedDate
+        onClickListenerInit()
+
+    }
+    private fun onClickListenerInit() {
         binding.backButton.setOnClickListener {
             (activity as MainActivity).replaceFragment(calendarFragment)
         }
         binding.checkButton.setOnClickListener {
             (activity as MainActivity).replaceFragment(calendarFragment)
         }
+        binding.happySeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.happyPercentTextView.text = "${p1}%"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+        binding.pleasureSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.pleasurePercentTextView.text = "${p1}%"
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+        binding.sadSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.sadPercentTextView.text = "${p1}%"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+        binding.depressedSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.depressedPercentTextView.text = "${p1}%"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+        binding.angerSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.angerPercentTextView.text = "${p1}%"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
     }
 }
